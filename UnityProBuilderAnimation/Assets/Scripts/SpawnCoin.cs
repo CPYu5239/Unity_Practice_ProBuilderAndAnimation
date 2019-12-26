5e3cda54ab5a56b4a21a29c75cc0ab7ce0bcd271
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-26 16:48:34 +0800

    完成金幣生成

---
+++ UnityProBuilderAnimation/Assets/Scripts/SpawnCoin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnCoin : MonoBehaviour
+{
+    [Header("金幣")]
+    public GameObject coin;
+    private AudioSource aud;
+    [Header("音效")]
+    public AudioClip clip;
+
+    private void Start()
+    {
+        aud = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void Spawn()
+    {
+        Instantiate(coin, new Vector3(0,0,0), Quaternion.identity);
+        aud.PlayOneShot(clip);
+    }
+}
